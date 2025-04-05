ca7cf61c55899a64a786c523fe98292ee8d244b0
Andrew Levinton <alevinton151@gmail.com>
2025-04-05 16:10:36 -0400

    debug debug

--- STATS/pages/June_2024_Regents.py
+++ STATS/pages/June_2024_Regents.py
@@ -1,14 +1,15 @@
 import streamlit as st
+import os
 
 def main():
     col1, col2 = st.columns(2)
     with col1:
         st.markdown("Question #34, June 2024 Regents")
-        st.image("images/Q34june24.png")
+        st.image(os.path.join(os.path.dirname(__file__), "Q34june24.png"))
         
     with col2:
         st.markdown("Score: 4 out of 4")
-        st.image("images/Q34ansjune24.png")
+        st.image(os.path.join(os.path.dirname(__file__), "Q34ansjune24.png"))
         
 
 if __name__ == "__main__":
