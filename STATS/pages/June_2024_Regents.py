7d091caa112776142d7dd6d01a5624a6dc4e929e
Andrew Levinton <alevinton151@gmail.com>
2025-04-05 16:05:23 -0400

    still trying

--- STATS/pages/June_2024_Regents.py
+++ STATS/pages/June_2024_Regents.py
@@ -4,11 +4,11 @@ def main():
     col1, col2 = st.columns(2)
     with col1:
         st.markdown("Question #34, June 2024 Regents")
-        st.image("pages/Q34june24.png")
+        st.image("images/Q34june24.png")
         
     with col2:
         st.markdown("Score: 4 out of 4")
-        st.image("pages/Q34ansjune24.png")
+        st.image("images/Q34ansjune24.png")
         
 
 if __name__ == "__main__":
