7d2b5a519613f1b9452b9adfc21a910185681934
Andrew Levinton <alevinton151@gmail.com>
2025-04-06 22:56:22 -0400

    reconfigured...

--- STATS/pages/June_2024_Regents.py
+++ STATS/pages/June_2024_Regents.py
@@ -5,11 +5,11 @@ def main():
     col1, col2 = st.columns(2)
     with col1:
         st.markdown("Question #34, June 2024 Regents")
-        st.image(os.path.join(os.path.dirname(__file__), "Q34june24.png"))
+        st.image("Q34june24.png")
         
     with col2:
         st.markdown("Score: 4 out of 4")
-        st.image(os.path.join(os.path.dirname(__file__), "Q34ansjune24.png"))
+        st.image("Q34ansjune24.png")
         
 
 if __name__ == "__main__":
