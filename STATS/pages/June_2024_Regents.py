3d55509c7e4cc4ec7c9188939a072779ea022e57
Andrew Levinton <alevinton151@gmail.com>
2025-04-08 17:05:54 -0400

    still understanding repo config

--- STATS/pages/June_2024_Regents.py
+++ STATS/pages/June_2024_Regents.py
@@ -5,12 +5,11 @@ def main():
     col1, col2 = st.columns(2)
     with col1:
         st.markdown("Question #34, June 2024 Regents")
-        st.image("Q34june24.png")
+        st.image("../images/Q34june24.png")  # Updated relative path
         
     with col2:
         st.markdown("Score: 4 out of 4")
-        st.image("Q34ansjune24.png")
-        
+        st.image("../images/Q34ansjune24.png")  # Updated relative path
 
 if __name__ == "__main__":
     main()
